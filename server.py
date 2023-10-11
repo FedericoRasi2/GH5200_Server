4d20226eae13c4a610983c420e099a6862b7e93c
Federico Rasi <federicorasigithubtest@gmail.com>
2023-10-11 09:51:22 +0100

    Update server.py

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import socket
 
 # Configura l'indirizzo IP e la porta del tuo server
 HOST = '0.0.0.0'  # Puoi utilizzare '0.0.0.0' per ascoltare su tutte le interfacce di rete
-PORT = 12345  # Sostituisci con la porta desiderata
+PORT = 8080  # Sostituisci con la porta desiderata
 
 # Byte di risposta per accettare i dati
 response_byte = b'\x01'  # Cambia il byte di risposta se necessario
